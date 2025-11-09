8157c36cbf829410cba6bc167b307a8769c61170
Boburjon2142 <boburjonabduganiyev83@gmail.com>
2025-11-09 21:07:39 +0500

    Added auto migration to render.yaml

--- klinika_project/settings.py
+++ klinika_project/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
+import dj_database_url # type: ignore
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
